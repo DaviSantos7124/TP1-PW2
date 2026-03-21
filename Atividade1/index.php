78faf44b0ec6bda81972d3db923f3f3bb8649f10
Davi Santos <davisantosinacioaraujo10@gmail.com>
2026-03-20 22:49:17 -0300

    Add files via upload

---
+++ Atividade1/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+    
+    
+    
+    ?>
+    <form action="result.php" method="post">
+    <label for="name">Name:</label>
+    <input type="text" name="name" id="name"><br>
+    <label for="email">E-mail:</label>
+    <input type="text" name="email" id="email"><br>
+    <input type="submit" value="Submit">
+</form>
+</body>
+</html>
